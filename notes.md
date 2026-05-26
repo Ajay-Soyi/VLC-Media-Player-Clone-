2baaedbd40fa62aea6ced7e6da3502019a2c16a6
Ajay <ajaysoyi4@gmail.com>
2026-05-26 13:19:01 +0530

    timeline functionalites and time updation done.

---
+++ notes.md
@@ -0,0 +1,3 @@
+## timeupdate: is an event provided by the HTML <video> and <audio> elements... tells whenever time is updated in the corresponding video or audio.
+
+## timer.addEventListener("input",timelineInteraction)...used in the range function to find out the value dynamically selected by the user.
--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const speedUp = document.getElementById("speedUp");
 const speedDown = document.getElementById("speedDown");
 const volumeUp = document.getElementById("volumeUp");
 const volumeDown = document.getElementById("volumeDown");
+const toast = document.querySelector(".toast");
+const timer = document.getElementById("timer");
 
 const fileAttached = () => {
   inputElement.click();
@@ -21,11 +23,75 @@ const playVideo = (obj) => {
   videoElem.src = link;
   videoArea.appendChild(videoElem);
   videoElem.play();
-  videoElem.controls = true;
+  // videoElem.controls = true;
+
+  // logic for time update
+  const currTime = document.getElementById("current-time");
+  const endTime = document.getElementById("end-time");
+
+  const updateTime = () => {
+    // updating time
+    const hour = Math.floor(videoElem.currentTime / 3600)
+      .toString()
+      .padStart(2, "0");
+    const remainingSec = videoElem.currentTime % 3600;
+    const min = Math.floor(remainingSec / 60)
+      .toString()
+      .padStart(2, "0");
+    const sec = Math.floor(remainingSec % 60)
+      .toString()
+      .padStart(2, "0");
+    currTime.textContent = `
+    ${hour}:${min}:${sec}`;
+  };
+
+  const endTimeUpdateAndTimeline = () => {
+    // end time udpate
+    const duration = videoElem.duration;
+    const hour = Math.floor(duration / 3600)
+      .toString()
+      .padStart(2, "0");
+    const remainingSec = duration % 3600;
+    const min = Math.floor(remainingSec / 60)
+      .toString()
+      .padStart(2, "0");
+    const sec = Math.floor(remainingSec % 60)
+      .toString()
+      .padStart(2, "0");
+    endTime.textContent = `
+      ${hour}:${min}:${sec}
+    `;
+
+    // timeline update
+    const timelineUpdate = () => {
+      timer.value = (videoElem.currentTime / videoElem.duration) * 100;
+    };
+    videoElem.addEventListener("timeupdate", timelineUpdate);
+
+    // timeline user interaction
+    const timelineInteraction = () => {
+      videoElem.currentTime = (timer.value / 100) * videoElem.duration;
+      timer.value = (videoElem.currentTime / videoElem.duration) * 100;
+    };
+
+    timer.addEventListener("input", timelineInteraction);
+  };
+
+  videoElem.addEventListener("timeupdate", updateTime);
+  videoElem.addEventListener("loadedmetadata", endTimeUpdateAndTimeline);
 };
 
 openOption.addEventListener("click", fileAttached);
 inputElement.addEventListener("change", playVideo);
+
+// toast logic
+const showToast = (message) => {
+  toast.style.display = "block";
+  toast.textContent = message;
+  setTimeout(() => {
+    toast.style.display = "none";
+  }, 2000);
+};
 
 // logic for speed and volume.
 const speedInc = () => {
@@ -34,6 +100,7 @@ const speedInc = () => {
     return;
   }
   videoElem.playbackRate = videoElem.playbackRate + 0.5;
+  showToast(`${videoElem.playbackRate}X`);
 };
 const speedDec = () => {
   const videoElem = document.querySelector("video");
@@ -41,6 +108,7 @@ const speedDec = () => {
     rerturn;
   }
   videoElem.playbackRate = videoElem.playbackRate - 0.5;
+  showToast(`${videoElem.playbackRate}X`);
 };
 const volumeInc = () => {
   const videoElem = document.querySelector("video");
@@ -52,6 +120,7 @@ const volumeInc = () => {
     return;
   }
   videoElem.volume = videoElem.volume + 0.1;
+  showToast(`${videoElem.volume * 100}%`);
 };
 const volumeDec = () => {
   const videoElem = document.querySelector("video");
@@ -62,6 +131,7 @@ const volumeDec = () => {
     return;
   }
   videoElem.volume = videoElem.volume - 0.1;
+  showToast(`${videoElem.volume * 100}%`);
 };
 speedUp.addEventListener("click", speedInc);
 speedDown.addEventListener("click", speedDec);
